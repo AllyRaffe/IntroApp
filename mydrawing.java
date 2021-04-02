11d880c6362fcbc39fd609a090267ac63428b900
AllyRaffe <72402276+AllyRaffe@users.noreply.github.com>
2021-04-02 11:15:34 -0600

    Create mydrawing.java

---
+++ mydrawing.java
@@ -0,0 +1,43 @@
+package com.example.introapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MyDrawing extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_drawing);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            case R.id.mnuMain:
+                startActivity(new Intent(getApplicationContext(), MainMenuActivity.class));
+                return true;
+            case R.id.mnuExit:
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+}
